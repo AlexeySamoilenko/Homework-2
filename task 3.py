84a8baa5a95efbdaa75a3039727508894bdf5f71
Alexey Samoilenko <a2.samoilenko@gmail.com>
2020-02-14 15:23:51 +0300

    Lock() is replaced by Rlock(), comments added

--- task 3.py
+++ task 3.py
@@ -1,12 +1,14 @@
-from threading import Thread, Lock
+from threading import Thread, RLock
+import time
 
-lock = Lock()
+
 a = 0
-
+lock = RLock()
 
 def function(arg):
     global a
     lock.acquire()
+    #a += arg <- much faster)
     for _ in range(arg):
         a += 1
     lock.release()
@@ -14,16 +16,18 @@ def function(arg):
 def main():
     threads = []
     for i in range(5):
-        i = Thread(target=function, args=(1000000,))
-        i.start()
-        threads.append(i)
-
-  
-    
-    
+        thread = Thread(target=function, args=(1000000,))
+        thread.start()
+        threads.append(thread)
         
     [t.join() for t in threads]
     print("----------------------", a)  # ???
 
 
 main()
+
+''' with args=(10000000,)
+    without lock      5.983 seconds
+    with lock         6.895 seconds
+    with Rlock        6.068 seconds
+    without for: a+=1 0.018 seconds'''
