25d04de353a707e7776f4428df52e1294cea5351
Alexey Samoilenko <a2.samoilenko@gmail.com>
2020-02-19 03:02:38 +0300

    added functions for sql requests

--- Task 4/SQL logic.py
+++ Task 4/SQL logic.py
@@ -1,24 +1,26 @@
-#Create table
-DROP TABLE IF EXISTS students;
-DROP TABLE IF EXISTS rooms;
 
-CREATE TABLE rooms(
-    id INT PRIMARY KEY,
-    name VARCHAR(20) NOT NULL
-) ENGINE=INNODB;
- 
-CREATE TABLE students(
-    id INT PRIMARY KEY,
-    name varchar(100) not null,
-    sex varchar(1) not null,
-    birthday DATETIME not null,
-    room INT null, 
-    CONSTRAINT fk_room
-    FOREIGN KEY (room) 
-        REFERENCES rooms(id)
-) ENGINE=INNODB;
+def drop_tables():
+    queries = '''DROP TABLE IF EXISTS students;
+                DROP TABLE IF EXISTS rooms;'''
 
-# filling from csv
+def create_tables():
+    queries = '''CREATE TABLE rooms(
+                    id INT PRIMARY KEY,
+                    name VARCHAR(20) NOT NULL
+                    ) ENGINE=INNODB;
+                CREATE TABLE students(
+                    id INT PRIMARY KEY,
+                    name varchar(100) not null,
+                    sex varchar(1) not null,
+                    birthday DATETIME not null,
+                    room INT null, 
+                    CONSTRAINT fk_room
+                    FOREIGN KEY (room) 
+                        REFERENCES rooms(id)
+                ) ENGINE=INNODB;'''
+
+def filling_from_csv():
+    queries =""
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/stu.CSV'
 INTO TABLE students
 FIELDS TERMINATED BY ',' 
@@ -29,36 +31,42 @@ IGNORE 1 ROWS
 
 SHOW VARIABLES LIKE 'secure_file_priv';
 
-select * from rooms;
-select * from students;
 
-#Stud in room
-SELECT rooms.name AS 'Room', COUNT(*) AS 'Students Count'
-FROM rooms JOIN students ON rooms.id = students.room
-GROUP BY rooms.name;
+def show_tables():
+    queries = '''
+    select * from rooms;
+    select * from students;'''
 
-#top avg age
-SET @currentYear = YEAR(NOW());
+def stud_in_room():
+    queries = '''SELECT rooms.name AS 'Room', COUNT(*) AS 'Students Count'
+                FROM rooms JOIN students ON rooms.id = students.room
+                GROUP BY rooms.name;'''
 
-SELECT rooms.name AS 'Room', AVG(@currentYear - year(students.birthday)) AS 'Average Age'
-FROM rooms JOIN students ON rooms.id = students.room
-GROUP BY rooms.name
-ORDER BY 2
-LIMIT 5;
+def top_avg_age():
+    queries = '''SET @currentYear = YEAR(NOW());
 
-#biggest dif
-SET @currentYear = YEAR(NOW());
+                SELECT rooms.name AS 'Room', AVG(@currentYear - year(students.birthday)) AS 'Average Age'
+                FROM rooms JOIN students ON rooms.id = students.room
+                GROUP BY rooms.name
+                ORDER BY 2
+                LIMIT 5;'''
 
-SELECT rooms.name AS 'Room', (MAX(@currentYear - year(students.birthday)) - MIN(@currentYear - year(students.birthday))) AS 'Min Age'
-FROM rooms JOIN students ON rooms.id = students.room
-GROUP BY rooms.name
-ORDER BY 2 DESC
-LIMIT 5;
+def biggest_dif():
+    queries = '''SET @currentYear = YEAR(NOW());
+    
+                SELECT rooms.name AS 'Room', (MAX(@currentYear - year(students.birthday)) - MIN(@currentYear - year(students.birthday))) AS 'Min Age'
+                FROM rooms JOIN students ON rooms.id = students.room
+                GROUP BY rooms.name
+                ORDER BY 2 DESC
+                LIMIT 5;'''
 
-#only two genders
-SELECT rooms.name AS 'Room'
-FROM rooms
-WHERE (SELECT COUNT(DISTINCT sex) FROM students WHERE rooms.id = students.room) > 1;
+def only_two_genders():
+    queries = '''SELECT rooms.name AS 'Room'
+                FROM rooms
+                WHERE (SELECT COUNT(DISTINCT sex) FROM students WHERE rooms.id = students.room) > 1;'''
 
-
-
+def list_of_request():
+    requests = [stud_in_room(),
+                top_avg_age(),
+                biggest_dif(),
+                only_two_genders()]
