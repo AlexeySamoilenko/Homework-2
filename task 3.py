115717db7d250c4e06211be7c72dde9b01813368
Alexey Samoilenko <a2.samoilenko@gmail.com>
2020-02-19 09:58:41 +0300

    Locks returned, process pool removed

--- task 3.py
+++ task 3.py
@@ -1,31 +1,33 @@
-from concurrent.futures import ProcessPoolExecutor as PoolExecutor
+from threading import Thread, RLock
 
+
+a = 0
+lock = RLock()
 
 def function(arg):
-    a = 0
+    global a
+    lock.acquire()
     for _ in range(arg):
-       a += 1
-    return a
+        a += 1
+    lock.release()
     
+
 def main():
-    a = 0
     threads = []
-    with PoolExecutor(max_workers=5) as executor:
-        #for _ in executor.map(function(1000000,),range(5)):
-        for _ in range(5):
-            a += function(1000000,)
-        
-    #[t.join() for t in threads]
+    for i in range(5):
+        thread = Thread(target=function, args=(1000000,))
+        thread.start()
+        threads.append(thread)
+         
+    s=[t.join() for t in threads]
     print("----------------------", a) # ???
 
 
 main()
 
 ''' with args=(10000000,) avr for 5 time
-    withPoolExecutor  4.031 seconds
     without lock      5.983 seconds
     with lock         6.895 seconds
     with Rlock        6.068 seconds
     without for: a+=1 0.018 seconds
     without threads   4.310 seconds'''
-
