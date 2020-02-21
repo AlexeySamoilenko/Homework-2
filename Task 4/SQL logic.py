1310475024c46121b0c75af78d346bcb62f1af4b
Alexey Samoilenko <a2.samoilenko@gmail.com>
2020-02-21 04:28:11 +0300

    Query syntax is fixed, cleaned from garbage

--- Task 4/SQL logic.py
+++ Task 4/SQL logic.py
@@ -1,14 +1,15 @@
-
 def drop_tables():
-    queries = '''DROP TABLE IF EXISTS students;
-                DROP TABLE IF EXISTS rooms;'''
+    queries = ('''DROP TABLE IF EXISTS students;''',
+               '''DROP TABLE IF EXISTS rooms;''')
+    return queries
+
 
 def create_tables():
-    queries = '''CREATE TABLE rooms(
+    queries = ('''CREATE TABLE rooms(
                     id INT PRIMARY KEY,
                     name VARCHAR(20) NOT NULL
-                    ) ENGINE=INNODB;
-                CREATE TABLE students(
+                    ) ENGINE=INNODB;''',
+                '''CREATE TABLE students(
                     id INT PRIMARY KEY,
                     name varchar(100) not null,
                     sex varchar(1) not null,
@@ -17,30 +18,22 @@ def create_tables():
                     CONSTRAINT fk_room
                     FOREIGN KEY (room) 
                         REFERENCES rooms(id)
-                ) ENGINE=INNODB;'''
-
-def filling_from_csv():
-    queries =""
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/stu.CSV'
-INTO TABLE students
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(birthday, id, name, room, sex);
-
-SHOW VARIABLES LIKE 'secure_file_priv';
+                ) ENGINE=INNODB;''')
+    return queries
 
 
 def show_tables():
-    queries = '''
-    select * from rooms;
-    select * from students;'''
+    queries = ('select * from rooms;'
+               'select * from students;')
+    return queries
+
 
 def stud_in_room():
     queries = '''SELECT rooms.name AS 'Room', COUNT(*) AS 'Students Count'
                 FROM rooms JOIN students ON rooms.id = students.room
                 GROUP BY rooms.name;'''
+    return queries
+
 
 def top_avg_age():
     queries = '''SET @currentYear = YEAR(NOW());
@@ -50,6 +43,8 @@ def top_avg_age():
                 GROUP BY rooms.name
                 ORDER BY 2
                 LIMIT 5;'''
+    return queries
+
 
 def biggest_dif():
     queries = '''SET @currentYear = YEAR(NOW());
@@ -59,14 +54,32 @@ def biggest_dif():
                 GROUP BY rooms.name
                 ORDER BY 2 DESC
                 LIMIT 5;'''
+    return queries
+
 
 def only_two_genders():
     queries = '''SELECT rooms.name AS 'Room'
                 FROM rooms
                 WHERE (SELECT COUNT(DISTINCT sex) FROM students WHERE rooms.id = students.room) > 1;'''
+    return queries
 
-def list_of_request():
-    requests = [stud_in_room(),
+
+def list_of_queries():
+    list_of_queries = [stud_in_room(),
                 top_avg_age(),
                 biggest_dif(),
                 only_two_genders()]
+    return list_of_queries
+
+
+def inserting_data():
+    queries = {
+        'rooms': 'INSERT INTO Rooms (id, name) VALUES (%s%s)',
+        'students': 'INSERT INTO Students (id, name, room, birthday, sex) VALUES (%s%s%s%s%s)'
+    }
+    return queries
+
+
+def insert_data():
+    # Inserting Multiple Rows
+    cursor.executemany(inserting_data, data)
